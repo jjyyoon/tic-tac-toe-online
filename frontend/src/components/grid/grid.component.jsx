02c2cbcfb359504fa5087e2d7110c9e1edc1d2b8
Jiyeon Yoon <jjjy.yoon@gmail.com>
2020-01-18 19:18:06 +0000

    Added a function to block the other player to play when it's not his turn

--- frontend/src/components/grid/grid.component.jsx
+++ frontend/src/components/grid/grid.component.jsx
@@ -7,39 +7,37 @@ class Grid extends React.Component {
   constructor(props) {
     super(props);
 
-    const createGrid = size => {
-      let grid = [];
-
-      for (let i = 0; i < size; i = i + 1) {
-        let row = [];
-
-        for (let j = 0; j < size; j = j + 1) {
-          row.push("");
-        }
-
-        grid.push(row);
-      }
-
-      return grid;
-    };
-
     this.state = {
-      grid: createGrid(props.size)
+      grid: this.createGrid(props.size)
     };
   }
+
+  createGrid = size => {
+    let grid = [];
+    for (let i = 0; i < size; i = i + 1) {
+      let row = [];
+      for (let j = 0; j < size; j = j + 1) {
+        row.push("");
+      }
+      grid.push(row);
+    }
+
+    return grid;
+  };
 
   updateState = (i, j, mark) => {
     const newGrid = this.state.grid.slice();
     newGrid[i][j] = mark;
 
     this.setState({ grid: newGrid });
-    console.log(this.state.grid);
+    this.props.changeTurn();
   };
 
   validate = () => {};
 
   render() {
     const { grid } = this.state;
+    const { player, currentTurn } = this.props;
 
     return (
       <div>
@@ -48,7 +46,8 @@ class Grid extends React.Component {
             {row.map((col, colIdx) => (
               <Space
                 key={`${rowIdx}${colIdx}`}
-                player="player1"
+                player={player}
+                currentTurn={currentTurn}
                 update={this.updateState}
                 validate={this.validate}
                 i={rowIdx}
--- frontend/src/components/space/space.component.jsx
+++ frontend/src/components/space/space.component.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import "./space.styles.scss";
 
-const Space = ({ player, update, validate, i, j }) => {
+const Space = ({ player, currentTurn, update, validate, i, j }) => {
   const mark = player === "player1" ? "O" : "X";
   const handleClick = event => {
-    event.target.innerHTML = mark;
-    update(i, j, mark);
-    validate();
+    if (player === currentTurn) {
+      event.target.innerHTML = mark;
+      update(i, j, mark);
+      validate();
+    }
   };
 
   return <div className="space" onClick={handleClick}></div>;
