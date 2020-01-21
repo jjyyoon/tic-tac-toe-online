69fe9b8b3160e6b48475596978847edcf99e8dcf
Jiyeon Yoon <jjjy.yoon@gmail.com>
2020-01-21 20:38:09 +0000

    In main.py file, changed where redirect to when a user sign up

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models.database import db
 from models.user import User
+from configuration import postgresql_uri
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = postgresql_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -29,7 +30,7 @@ def createuser():
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for("index"))
+    return redirect(url_for("list"))
 
 
 if __name__ == "__main__":
