70d4eca3eb1b0287a7bef625f82a1d433f5422f5
Jiyeon Yoon <jjjy.yoon@gmail.com>
2020-01-28 23:22:27 +0000

    Applied foreign keys to columns of players, etc in the game and room tables

--- backend/models/game.py
+++ backend/models/game.py
@@ -3,7 +3,7 @@ from .database import db
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    player1 = db.Column(db.String(80), nullable=False)
-    player2 = db.Column(db.String(80), nullable=False)
-    winner = db.Column(db.String(80), nullable=False)
-    loser = db.Column(db.String(80), nullable=False)
+    player1 = db.Column(db.String(80), db.ForeignKey('user.username'))
+    player2 = db.Column(db.String(80), db.ForeignKey('user.username'))
+    winner = db.Column(db.String(80), db.ForeignKey('user.username'))
+    loser = db.Column(db.String(80), db.ForeignKey('user.username'))
--- backend/models/room.py
+++ backend/models/room.py
@@ -3,6 +3,6 @@ from .database import db
 
 class Room(db.Model):
     id = db.Column(db.String(50), primary_key=True)
-    password = db.Column(db.String(125), nullable=False)
-    player1 = db.Column(db.String(80), nullable=False)
-    player2 = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(125))
+    player1 = db.Column(db.String(80), db.ForeignKey('user.username'))
+    player2 = db.Column(db.String(80), db.ForeignKey('user.username'))
