47c8caca8fd4b8835b44da56c961b4476d3a4cdf
Jiyeon Yoon <jjjy.yoon@gmail.com>
2020-02-09 17:52:39 +0000

    Let the database generate UUIDs

--- backend/models/game.py
+++ backend/models/game.py
@@ -1,8 +1,10 @@
 from .database import db
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
 
 
 class Game(db.Model):
-    id = db.Column(db.String(50), primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4())
     state = db.Column(db.Text, nullable=False)
     player1_username = db.Column(
         db.String(80), db.ForeignKey('user.username'), nullable=False)
--- backend/models/room.py
+++ backend/models/room.py
@@ -1,9 +1,11 @@
 from .database import db
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
 
 
 class Room(db.Model):
-    id = db.Column(db.String(50), primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4())
     name = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(60))
     created_by = db.Column(db.String(80), db.ForeignKey(
